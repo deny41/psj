340e78993a951c01ea5b35e09aed2c4a4630b468
AzrieL/Muttq <denyprasetyo41@gmail.com>
2018-11-14 16:33:41 +0700

    First commit

---
+++ resources/views/layouts_backend/_css_backend.blade.php
@@ -0,0 +1,5 @@
+<!-- Custom CSS -->
+<link href="{{ asset('assets/extra-libs/c3/c3.min.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/libs/morris.js/morris.css') }}" rel="stylesheet">
+<!-- Custom CSS -->
+<link href="{{ asset('dist/css/style.min.css') }}" rel="stylesheet">
---
+++ resources/views/layouts_backend/_scripts_backend.blade.php
@@ -0,0 +1,33 @@
+<script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="{{ asset('assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
+<script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<!-- apps -->
+<script src="{{ asset('dist/js/app.min.js') }}"></script>
+<script src="{{ asset('dist/js/app.init.js') }}"></script>
+<script src="{{ asset('dist/js/app-style-switcher.js') }}"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="{{ asset('assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js') }}"></script>
+<script src="{{ asset('assets/extra-libs/sparkline/sparkline.js') }}"></script>
+<!--Wave Effects -->
+<script src="{{ asset('dist/js/waves.js') }}"></script>
+<!--Menu sidebar -->
+<script src="{{ asset('dist/js/sidebarmenu.js') }}"></script>
+<!--Custom JavaScript -->
+<script src="{{ asset('dist/js/custom.min.js') }}"></script>
+<!--This page JavaScript -->
+<!--chartis chart-->
+<script src="{{ asset('assets/libs/chartist/dist/chartist.min.js') }}"></script>
+<script src="{{ asset('assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js') }}"></script>
+<!--c3 charts -->
+<script src="{{ asset('assets/extra-libs/c3/d3.min.js') }}"></script>
+<script src="{{ asset('assets/extra-libs/c3/c3.min.js') }}"></script>
+<!--chartjs -->
+<script src="{{ asset('assets/libs/raphael/raphael.min.js') }}"></script>
+<script src="{{ asset('assets/libs/morris.js/morris.min.js') }}"></script>
+
+<script src="{{ asset('dist/js/pages/dashboards/dashboard1.js') }}"></script>
+
+<script type="text/javascript">
+    $(".preloader").fadeOut();
+</script>
